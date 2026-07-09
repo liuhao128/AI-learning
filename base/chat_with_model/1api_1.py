a545380c50d6465aff68ab1405335f300e9051c6
liuhao <669526720@qq.com>
2026-07-09 17:10:33 +0800

    测试chatBi

--- base/chat_with_model/1api_1.py
+++ base/chat_with_model/1api_1.py
@@ -11,23 +11,32 @@ client = OpenAI(
 )
 
 # 2.调用模型 5c
+# completion = client.chat.completions.create(
+#     # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
+#     model = "qwen3-max",
+#     messages = [
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "你的模型具体名字是qwen3.7-max吗"},
+#     ],
+#     stream=True,
+# )
+
 completion = client.chat.completions.create(
     # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
-    model = "qwen-plus",
+    model = "qwen3-max",
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "你的模型具体名字是qwen3.7-max吗"},
-    ],
-    stream=True,
+        {"role": "user", "content": "你的模型具体名字是什么"},
+    ]
 )
 
 # # 3.打印输出
 # print(completion.model_dump_json())
 # # 3.1.只打印内容
-# print(completion.choices[0].message.content)
+print(completion.choices[0].message.content)
 
 # 4.流示打印
-for chunk in completion:
-    content = chunk.choices[0].delta.content
-    if content:
-        print(content, end="", flush=True)  # end="" 避免换行，flush=True 强制立即输出
+# for chunk in completion:
+#     content = chunk.choices[0].delta.content
+#     if content:
+#         print(content, end="", flush=True)  # end="" 避免换行，flush=True 强制立即输出
